e6db9bd8b43688eb7f3c3d9757a3ca1daed4cd14
hoggebi <wdk2001@naver.com>
2026-08-22 21:15:34 +0900

    Add Chapter type with properties for id and pages

--- lib/types.ts
+++ lib/types.ts
@@ -9,3 +9,9 @@ export type ExtractedPage = {
   title: string;
   sentences: string[];
 };
+export type Chapter = {
+  id: string;
+  title: string;
+  pages: ExtractedPage[];
+  createdAt: number;
+};
